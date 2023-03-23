4d422250029404eb3c4c2b774a5bcc7820ac8d41
Darshan Makwana <darshanmakwana412@gmail.com>
2023-03-23 23:10:53 +0530

    edges implementation

--- graph.h
+++ graph.h
@@ -1,15 +1,26 @@
+#include <vector>
+using namespace std;
+
 class Node
 {
 private:
     int color = 0;
     Node* p = nullptr;
+    vector<Node*> neighbours;
 public:
-    Node(/* args */);
+    Node(const int& _color, Node*& _p);
     ~Node();
+
+    void addNeighbour(Node* n) {
+        neighbours.push_back(n);
+    }
+
 };
 
-Node::Node(/* args */)
+Node::Node(const int& _color, Node*& _p)
 {
+    p = _p;
+    color = _color;
 }
 
 Node::~Node()
